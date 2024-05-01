5060d787c40e661a579f45192cc6d62eec0af36a
dyahnrchls <dyahnrchls@gmail.com>
2024-05-02 06:54:55 +0700

    initial shadcn ui.

---
+++ src/pages/home/Home.util.ts
@@ -0,0 +1,31 @@
+import { useCallback, useEffect } from "react";
+import { fetchUserLists, userListSelector } from "../../store/slices";
+import { useAppDispatch, useAppSelector } from "../../store";
+
+export const useHomeUtil = (navigation: any) => {
+  const dispatch = useAppDispatch();
+  const users = useAppSelector(userListSelector);
+
+  const handleNavigateToDetail = (id: string) => {
+    navigation?.navigate("Detail", { id });
+  };
+
+  const handleNavigateToAdd = () => {
+    navigation?.navigate("Add");
+  };
+
+  const getUserList = useCallback(() => {
+    dispatch(fetchUserLists());
+  }, [dispatch]);
+
+  useEffect(() => {
+    getUserList();
+  }, [getUserList]);
+
+  return {
+    handleNavigateToDetail,
+    handleNavigateToAdd,
+    users,
+    getUserList,
+  };
+};
