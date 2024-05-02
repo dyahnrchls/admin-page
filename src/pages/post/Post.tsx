79f31d59e6a9616643342443db14f3026bc2d992
dyahnrchls <dyahnrchls@gmail.com>
2024-05-02 10:20:02 +0700

    Integration create, update, delete post.

--- src/pages/post/Post.tsx
+++ src/pages/post/Post.tsx
@@ -6,28 +6,158 @@ import {
   CardTitle,
 } from "src/@/components/ui/card";
 import { usePostUtil } from "./Post.util";
+import { Button } from "src/@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "src/@/components/ui/dialog";
+import { Input } from "src/@/components/ui/input";
+import { Label } from "src/@/components/ui/label";
 
 export const PostPage = () => {
-  const { data } = usePostUtil();
+  const { data, setTitle, setBody, onCreate, setPostId, onUpdate, onDelete } =
+    usePostUtil();
 
   return (
-    <div className="flex justify-center my-6">
-      <div className="flex flex-col gap-4">
-        {data?.map(
-          (item: {
-            id: number;
-            userId: number;
-            title: string;
-            body: string;
-          }) => (
-            <Card key={item?.id} className="w-[50rem] cursor-pointer">
-              <CardHeader>
-                <CardTitle>{item?.title}</CardTitle>
-                <CardDescription>{item?.body}</CardDescription>
-              </CardHeader>
-            </Card>
-          )
-        )}
+    <div>
+      <div className="mt-6 flex justify-center">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>Add Post</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Add Post</DialogTitle>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  title
+                </Label>
+                <Input
+                  id="title"
+                  className="col-span-3"
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  body
+                </Label>
+                <Input
+                  id="body"
+                  className="col-span-3"
+                  onChange={(e) => setBody(e.target.value)}
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit" onClick={onCreate}>
+                Add Post{" "}
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>
+      <div className="flex justify-center my-6">
+        <div className="flex flex-col gap-4">
+          {data?.map(
+            (item: {
+              id: number;
+              userId: number;
+              title: string;
+              body: string;
+            }) => (
+              <Card key={item?.id} className="w-[50rem] cursor-pointer">
+                <CardHeader>
+                  <CardTitle>{item?.title}</CardTitle>
+                  <CardDescription>{item?.body}</CardDescription>
+                  <div className="flex justify-end gap-4">
+                    <Dialog>
+                      <DialogTrigger asChild>
+                        <Button
+                          onClick={() => setPostId(item.id)}
+                          className="text-black bg-white border"
+                        >
+                          Edit
+                        </Button>
+                      </DialogTrigger>
+                      <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                          <DialogTitle>Edit Post</DialogTitle>
+                        </DialogHeader>
+                        <div className="grid gap-4 py-4">
+                          <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="name" className="text-right">
+                              Title
+                            </Label>
+                            <Input
+                              id="title"
+                              defaultValue={item.title}
+                              className="col-span-3"
+                              onChange={(e) => setTitle(e.target.value)}
+                            />
+                          </div>
+                          <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="username" className="text-right">
+                              Body
+                            </Label>
+                            <Input
+                              id="body"
+                              defaultValue={item.body}
+                              className="col-span-3"
+                              onChange={(e) => setBody(e.target.value)}
+                            />
+                          </div>
+                        </div>
+                        <DialogFooter>
+                          <Button type="submit" onClick={onUpdate}>
+                            Save changes
+                          </Button>
+                        </DialogFooter>
+                      </DialogContent>
+                    </Dialog>
+
+                    <Dialog>
+                      <DialogTrigger asChild>
+                        <Button>
+                          <Button onClick={() => setPostId(item.id)}>
+                            Delete
+                          </Button>
+                        </Button>
+                      </DialogTrigger>
+                      <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                          <DialogTitle>
+                            Are you sure want to delete this post?
+                          </DialogTitle>
+
+                          <DialogDescription>
+                            Title: {item.title}
+                          </DialogDescription>
+                          <DialogDescription>
+                            Body: {item.body}
+                          </DialogDescription>
+                        </DialogHeader>
+
+                        <DialogFooter>
+                          <Button type="submit" onClick={onDelete}>
+                            Delete
+                          </Button>
+                        </DialogFooter>
+                      </DialogContent>
+                    </Dialog>
+                  </div>
+                </CardHeader>
+              </Card>
+            )
+          )}
+        </div>
       </div>
     </div>
   );
--- src/pages/post/Post.util.ts
+++ src/pages/post/Post.util.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useState } from "react";
+import { useMutation, useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 
-const useGetPostList = (userId: string | undefined) =>
+const useGetPostList = (userId: string | undefined, queryKey: string) =>
   useQuery({
-    queryKey: ["post"],
+    queryKey: ["post", queryKey],
     queryFn: async () => {
       const { data } = await axios.get(
         `https://jsonplaceholder.typicode.com/users/${userId}/posts`
@@ -13,13 +14,82 @@ const useGetPostList = (userId: string | undefined) =>
     },
   });
 
+const useAddPost = () =>
+  useMutation({
+    mutationFn: (payload) =>
+      axios.post("https://jsonplaceholder.typicode.com/posts", payload),
+  });
+
+const useUpdatePost = (id: number | undefined) =>
+  useMutation({
+    mutationFn: (payload) =>
+      axios.patch(`https://jsonplaceholder.typicode.com/posts/${id}`, payload),
+  });
+
+const useDeletePost = (id: number | undefined) =>
+  useMutation({
+    mutationFn: () =>
+      axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`),
+  });
+
 export const usePostUtil = () => {
   const { userId } = useParams<{ userId: string }>();
 
-  const { status, data, error, isLoading } = useGetPostList(userId);
+  const [title, setTitle] = useState<string>("");
+  const [body, setBody] = useState<string>("");
+  const [postId, setPostId] = useState<number>();
+
+  const [queryKey, setQueryKey] = useState<string>("");
+
+  const { data, isLoading } = useGetPostList(userId, queryKey);
+
+  const addPost = useAddPost();
+  const updatePost = useUpdatePost(postId);
+  const deletePost = useDeletePost(postId);
+
+  const onCreate = () => {
+    const payload = {
+      title,
+      body,
+      userId: Number(userId),
+    };
+
+    addPost.mutate(payload as any, {
+      onSuccess: (resp) => setQueryKey(`newPost-${resp?.data?.id}`),
+    });
+  };
+
+  const onUpdate = () => {
+    const payload = {
+      id: postId,
+      title,
+      body,
+      userId: Number(userId),
+    };
+
+    updatePost.mutate(payload as any, {
+      onSuccess: (resp) => setQueryKey(`updatePost-${resp?.data?.id}`),
+    });
+  };
+
+  const onDelete = () => {
+    const payload = {};
+
+    deletePost.mutate(payload as any, {
+      onSuccess: () => setQueryKey(`deletePost-${postId}`),
+    });
+  };
 
   return {
     data,
     isLoading,
+    title,
+    setTitle,
+    body,
+    setBody,
+    onCreate,
+    onUpdate,
+    setPostId,
+    onDelete,
   };
 };
